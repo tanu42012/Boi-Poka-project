2aa9798de720320ff692df8f6a1f064631026903
tanu42012 <sanjibks03@gmail.com>
2025-04-18 14:12:10 -0700

    added new alert

--- src/Components/Pages/BookDetails/BookDetails.jsx
+++ src/Components/Pages/BookDetails/BookDetails.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { addToStoreDB } from '../../../Utility/addToDB.JS';
+// import Swal from 'sweetalert2'
+// import withReactContent from 'sweetalert2-react-content'
+// import React from 'react';
+
+ 
+
+// const MySwal = withReactContent(Swal)
+import { ToastContainer, toast } from 'react-toastify';
 
 const BookDetails = () => {
     const {id}=useParams();
@@ -12,7 +20,16 @@ const BookDetails = () => {
     // console.log(singleBook);
     // console.log( id,data);
     const {bookName,image}=singleBook || {};
+
+    
     const handleMarkAsRead=(id)=>{
+
+        // MySwal.fire({
+        //     title: "Good job!",
+        //     text: "You clicked the button!",
+        //     icon: "success"
+        //   });
+        toast("Wow so easy!");
         addToStoreDB(id)
         
 
@@ -22,6 +39,7 @@ const BookDetails = () => {
         <div className=' w-2/3'>
            <img className='w-48' src={image} alt="" />
            <h5>{bookName}</h5>
+           <ToastContainer />ssss
            <button onClick={()=>handleMarkAsRead(id)} className="btn btn-accent m-2">Mark asRead</button>
            <button className="btn btn-info m-2"> Add To wishlist</button>
         </div>
